fc1b4c344d3141235249d35b01e9ee940a90b7ba
rgsubramaniam <83640961+rgsubramaniam@users.noreply.github.com>
2024-12-28 23:00:27 +0000

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,11 @@
-from src.maths import add,mul
+from src.maths import add,sub
 
-def test_add(a,b):
+def test_add():
     assert add(2,3)==5
-    assert add(-1,2)==1
-
-def test_mul(a,b):
-    assert mul(4,1)==4
-    assert mul(3,2)==6
+    assert add(-1,1)==0
+    
+def test_sub():
+    assert sub(5,3)==2
+    assert sub(4,3)==1
+    assert sub(3,3)==0
+    assert sub(2,3)==-1
