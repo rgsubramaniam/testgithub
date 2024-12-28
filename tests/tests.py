d3cd9942f2bba19356864de5c4c982ecfc9ef01f
rgsubramaniam <83640961+rgsubramaniam@users.noreply.github.com>
2024-12-28 23:01:13 +0000

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -4,8 +4,6 @@ def test_add():
     assert add(2,3)==5
     assert add(-1,1)==0
     
-def test_sub():
-    assert sub(5,3)==2
-    assert sub(4,3)==1
-    assert sub(3,3)==0
-    assert sub(2,3)==-1
+def test_mul():
+    assert mul(5,3)==15
+    assert mul(2,3)==6
